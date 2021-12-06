c93b5692929ee581a12ac6e9f8b3a5c47e5f1819
di0xgenes <di0xgenes@protonmail.com>
2021-12-05 22:42:08 -0500

    simplify

--- background.js
+++ background.js
@@ -2,11 +2,19 @@ const contextMenu = chrome.contextMenus;
 
 contextMenu.create({
   id: 'decode',
-  title: 'Decode: %s',
+  title: 'Decode selected text',
   contexts: ['all']
 });
 
-const MORSE_CODE = {
+contextMenu.create({
+  id: 'encode',
+  title: 'Encode selected text',
+  contexts: ['all']
+});
+
+// Credit where it's due, MORSE CODE mappings:
+// https://stackoverflow.com/questions/43726344/js-decoding-morse-code
+const DECODE = {
   "-----":"0",
   ".----":"1",
   "..---":"2",
@@ -48,31 +56,66 @@ const MORSE_CODE = {
   "--..--":","
 };
 
-function translate(selectionText) {
-  const translatedWords = [];
-  const words = selectionText.split('/');
+const ENCODE = {
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "A": ".-",
+  "B": "-...",
+  "C": "-.-.",
+  "D": "-..",
+  "E": ".",
+  "F": "..-.",
+  "G": "--.",
+  "H": "....",
+  "I": "..",
+  "J": ".---",
+  "K": "-.-",
+  "L": ".-..",
+  "M": "--",
+  "N": "-.",
+  "O": "---",
+  "P": ".--.",
+  "Q": "--.-",
+  "R": ".-.",
+  "S": "...",
+  "T": "-",
+  "U": "..-",
+  "V": "...-",
+  "W": ".--",
+  "X": "-..-",
+  "Y": "-.--",
+  "Z": "--..",
+  "!": "-.-.--",
+  ".": ".-.-.-",
+  ":": "--..--",
+};
 
-  for (var i = 0; i < words.length; i++) {
-    const word = words[i].split(' ');
-    const translatedWord = [];
-
-    for (var character = 0; character < word.length; character++) {
-      translatedWord.push(MORSE_CODE[word[character]]);
-    }
-
-    translatedWords.push(translatedWord.join(''));
-  }
-
-  return translatedWords.join(' ');
+function decode(selectionText) {
+  return selectionText
+    .split('/')
+    .map(word => word.split(' ').map(character => DECODE[character.toUpperCase()]).join(''))
+    .join(' ');
 }
 
+function encode(selectionText) {
+  return selectionText
+    .split(' ')
+    .map(word => word.split('').map(character => ENCODE[character.toUpperCase()]).join(' '))
+    .join('/');
+}
 
 contextMenu.onClicked.addListener(function({ menuItemId, selectionText }, page) {
-  if (menuItemId === 'decode') {
-    const translated = translate(selectionText);
+  const translated = menuItemId === "encode" ? encode(selectionText) : decode(selectionText);
 
-    chrome.tabs.executeScript({
-      code: `alert('${translated}!')`
-    });
-  }
+  chrome.tabs.executeScript({
+    code: `alert('${translated}')`
+  });
 });
